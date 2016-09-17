89908f25fd960824c843304ef0b3a4418d71991d
swirllyman <swirllyman@gmail.com>
2016-09-17 15:02:14 -0700

    add files to repo

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/StartPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPosition : MonoBehaviour {
+
+    public int index;
+    public Transform buddyStart;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
