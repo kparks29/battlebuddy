c8f5b208b94908cffc09f3e58abe1e8bb043c845
swirllyman <swirllyman@gmail.com>
2016-09-18 00:49:21 -0700

    s

---
+++ Assets/Scripts/StartUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartUI : MonoBehaviour {
+
+    public Text code;
+
+    void Start()
+    {
+        code.text = "";
+    }
+
+	public void InputCode(int num)
+    {
+
+    }
+}
